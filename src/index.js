7dcdf6124b981c306c54d063e5bbef18f93fc9a9
sharuk uffair <sharukuffair1998@gmail.com>
2023-02-24 12:10:12 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import "./styles.css";
+
+let minusEle = document.querySelector(".minus");
+let plusEle = document.querySelector(".plus");
+let counterEle = document.querySelector(".counter");
+
+console.log(typeof counterEle.textContent);
+
+function increment() {
+  let value = parseInt(counterEle.textContent);
+  value = value + 1;
+  counterEle.textContent = value;
+}
+
+function decrement() {
+  let value = parseInt(counterEle.textContent);
+  value = value - 1;
+  counterEle.textContent = value;
+}
+
+minusEle.addEventListener("click", () => {
+  decrement();
+});
+
+plusEle.addEventListener("click", () => {
+  increment();
+});
